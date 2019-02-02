b070592c692794a4a36088a8f41e93da5b6fa31a
abelsco-fag <abel_menezes22@hotmail.com>
2019-02-02 09:59:33 -0200

    Atualizando

--- canais-HSV.py
+++ canais-HSV.py
@@ -1,1 +1,13 @@
 import cv2 as cv
+
+img = cv.imread('1.jpg')
+img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+matriz, saturacao, valor = cv.split(img)
+cv.imshow('Original', img)
+cv.imshow('Canal H', matriz)
+cv.imshow('Canal S', saturacao)
+cv.imshow('Canal V', valor)
+
+cv.waitKey(0)
+cv.destroyAllWindow()
